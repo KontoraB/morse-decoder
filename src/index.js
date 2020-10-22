fb121bdaca8a7434c39afdaa8a9035633fa75394
Oleksandr Boitsov <kontorab@gmail.com>
2020-10-22 21:19:37 +0300

    feat: add code in morse decoder and fix bugs

--- src/index.js
+++ src/index.js
@@ -42,9 +42,22 @@ function decode(expr) {
     //получаем из исходной строки expr массив строк где каждая строка состоит из 10 символов
    //т.е split(/(.{10})/).filter(O=>O) - нарезаем строки из expr длиной по 10 символов, и где текущее регулярное выражение
    //создает пустые элементы массива между кусками. filter(x=>x) используется для фильтрации этих пустых элементов
-    let exprArr = expr.split(/(.{10})/).filter(O=>O);
+    let exprArray = expr.split(/(.{10})/).filter(O=>O);
 
-    
+//кодируем в морзе - в задании указано 10 обозначает точку ( .), 11 обозначает тире ( -), 10 шт * будет пробел - меняем в цикле
+for(let i=0; i < exprArr.length; i++) {
+    exprArray[i] = exprArray[i].replace(/10/g, ".").replace(/11/g, "-").replace(/0/g, "").replace(/\*{10}/g, " ");
+}
+ // далее код морзе меняем на символы из MORSE_TABLE 
+ let result = [];
+ for(let i=0; i < exprArray.length; i++) {
+ for(const j in MORSE_TABLE) {
+     if (exprArray[i] === keys)  result[i] = MORSE_TABLE[j];
+     if (exprArray[i] === ' ') result[i] = ' ';
+ }
+ }
+ result = result.join('');
+ return result;
 }
 
 module.exports = {
